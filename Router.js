a8198592ebea28563dff978f530237ace0b52dd2
Brandon Kitajchuk <kitajchuk@gmail.com>
2016-03-22 09:37:35 -0700

    Add isFile condition for determining router action.

--- Router.js
+++ Router.js
@@ -341,7 +341,8 @@
                 isMatched = elem && this._matcher.test( elem.href ),
                 isIgnore = elem && elem.className.indexOf( "js-router--ignore" ) !== -1,
                 isMetaKey = elem && e.metaKey,
-                isBlank = elem && elem.target === "_blank";
+                isBlank = elem && elem.target === "_blank",
+                isFile = isDomain && elem && elem.href.split( "." ).pop() !== "";
             
             // 0.1 => Ensure url passes MatchRoute config
             // 0.2 => Ensure url is on the Document's Domain
@@ -349,7 +350,8 @@
             // 0.4 => Ensure the element does not contain a `js-router--ignore` className
             // 0.5 => Ensure the Event.metaKey is not TRUE - Command+click
             // 0.6 => Ensure the element target is not for a new tab
-            if ( isMatched && isDomain && !isHashed && !isIgnore && !isMetaKey && !isBlank ) {
+            // 0.7 => Ensure url is not a file link on the same document domain
+            if ( isMatched && isDomain && !isHashed && !isIgnore && !isMetaKey && !isBlank && !isFile ) {
                 this._preventDefault( e );
                 
                 if ( !_isRouting ) {
